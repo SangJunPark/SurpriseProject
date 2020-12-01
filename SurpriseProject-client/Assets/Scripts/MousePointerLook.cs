cb193e47d0206a0ee2380ea4ecd46adffea4b130
DESKTOP-9LM7VKK\user <jun89920@gmail.com>
2020-12-01 18:55:44 +0900

    aa

---
+++ SurpriseProject-client/Assets/Scripts/MousePointerLook.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePointerLook : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //LookerTR = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float midPoint = (transform.position - Camera.main.transform.position).magnitude * 0.5f;
+        Vector3 vec = mouseRay.origin + mouseRay.direction * midPoint;
+        vec.y = 0;
+        transform.LookAt(vec);
+    }
+}
--- SurpriseProject-client/Assets/Scripts/MovementController.cs
+++ SurpriseProject-client/Assets/Scripts/MovementController.cs
@@ -9,12 +9,14 @@ public class MovementController : MonoBehaviour
     Vector3 Direction;
     Animator animator;
     Transform CharacterTR;
+    Rigidbody CharacterRigidbody;
     Vector2 rotation = Vector2.zero;
     // Start is called before the first frame update
     void Start()
     {
         CharacterTR = GetComponent<Transform>();
         animator = GetComponent<Animator>();
+        CharacterRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -26,25 +28,44 @@ public class MovementController : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             Magnitude.z = Mathf.Clamp(Magnitude.z + 0.01f, -0.1f,0.1f);
+            Direction.z = 100;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             Magnitude.z = Mathf.Clamp(Magnitude.z - 0.01f, -0.1f, 0.1f);
+            Direction.z = -100;
         }
-        Magnitude.z = Magnitude.z > 0 ? Magnitude.z - Time.deltaTime*0.4f : Magnitude.z+ Time.deltaTime*0.4f;
-        Magnitude.z = Mathf.Abs(Magnitude.z) < 0.0001 ? 0 :  Magnitude.z;
-        PhysicalVelocity.z = Mathf.Min(PhysicalVelocity.z + Magnitude.z, 0.1f);
-        PhysicalVelocity.z = Mathf.Clamp(PhysicalVelocity.z -= Time.deltaTime, 0f, 0.1f);
-        CharacterTR.Translate(Magnitude);
 
-        animator.SetFloat("velocity", Mathf.Abs(Magnitude.z));
+        if (Input.GetKey(KeyCode.D))
+        {
+            Magnitude.x = Mathf.Clamp(Magnitude.z + 0.01f, -0.1f,0.1f);
+            Direction.x = 100;
+        }
 
-        
-            rotation.y += Input.GetAxis("Mouse X") * 5f;
-            rotation.x += Input.GetAxis("Mouse Y") * 5f;
-            rotation.x = Mathf.Clamp(rotation.x, -60, 60);
-            transform.localRotation = Quaternion.Euler(rotation.x, 0, 0);
-            transform.eulerAngles = new Vector2(0, rotation.y);
+        if (Input.GetKey(KeyCode.A))
+        {
+            Magnitude.x= Mathf.Clamp(Magnitude.z - 0.01f, -0.1f, 0.1f);
+            Direction.x = -100;
+        }
+        // Magnitude.z = Magnitude.z > 0 ? Magnitude.z - Time.deltaTime*0.4f : Magnitude.z+ Time.deltaTime*0.4f;
+        // Magnitude.z = Mathf.Abs(Magnitude.z) < 0.0001 ? 0 :  Magnitude.z;
+        // PhysicalVelocity.z = Mathf.Min(PhysicalVelocity.z + Magnitude.z, 0.1f);
+        // PhysicalVelocity.z = Mathf.Clamp(PhysicalVelocity.z -= Time.deltaTime, 0f, 0.1f);
+        // CharacterTR.Translate(Magnitude);
+
+            // rotation.y += Input.GetAxis("Mouse X") * 5f;
+            // rotation.x += Input.GetAxis("Mouse Y") * 5f;
+            // rotation.x = Mathf.Clamp(rotation.x, -60, 60);
+            // transform.localRotation = Quaternion.Euler(rotation.x, 0, 0);
+            // transform.eulerAngles = new Vector2(0, rotation.y);
+
+        animator.SetFloat("velocity", Mathf.Abs(Direction.magnitude));
+        CharacterRigidbody.AddRelativeForce(Direction);
+        //Debug.Log(CharacterRigidbody.velocity);
+
+        //Debug.Log(Input.GetAxis("Horizontal"));
+        //Debug.Log(Input.GetAxis("Mouse Y"));
+
     }
 }
