c2cc79e7b092d662205964b3dec5a87c9e7f6280
stzSangJunPark <sangjun.park@sundaytoz.com>
2020-12-26 17:03:39 +0900

    GG

---
+++ SurpriseProject-client/Assets/Scripts/HPComponent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPComponent : MonoBehaviour
+{
+    float MaxHP;
+    float CurrentHP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //adfasdf
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Notify(){
+
+    }
+}
---
+++ SurpriseProject-client/Assets/Scripts/ISPComponent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SPLevelObject
+{
+
+    abstract public void Notify();
+    virtual public void AddCallback(){
+
+    }
+    virtual public void RemoveCallback(){
+
+    }
+}
