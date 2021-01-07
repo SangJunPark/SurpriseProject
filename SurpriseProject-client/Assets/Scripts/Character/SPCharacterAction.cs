0a2881c0ebd55a35c0389a3eb30611764e900216
stzSangJunPark <sangjun.park@sundaytoz.com>
2021-01-07 20:04:24 +0900

    char action init

---
+++ SurpriseProject-client/Assets/Scripts/Character/SPCharacterAction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.TopDownEngine;
+
+public class SPCharacterAction : CharacterAbility
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
